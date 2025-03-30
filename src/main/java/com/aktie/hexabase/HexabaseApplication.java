60853fa081da1b3487264049fe4177f237f6b425
Sandrolaxx <sandro--ramos@hotmail.com>
2025-03-30 19:08:17 -0300

    create project

---
+++ src/main/java/com/aktie/hexabase/HexabaseApplication.java
@@ -0,0 +1,13 @@
+package com.aktie.hexabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexabaseApplication.class, args);
+	}
+
+}
